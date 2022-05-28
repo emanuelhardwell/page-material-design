0d57a0693adfa870d7d121c8dc79162940485c4e
emanuelhardwell <emanuelnoe16@gmail.com>
2022-05-28 13:51:07 -0500

    [UI] funcionalidad de modo oscuro creada

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { AppRouter } from "./routes/AppRouter";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Switch } from "@mui/material";
 
 export const App = () => {
+  const [isDark, setIsDark] = useState(false);
+
+  const theme = createTheme({
+    palette: {
+      mode: isDark ? "dark" : "light",
+      primary: {
+        main: "#F8D5AF",
+      },
+    },
+  });
+
   return (
     <>
-      <ThemeProvider>
+      <Switch onChange={(e) => setIsDark(!isDark)} />
+      <ThemeProvider theme={theme}>
         <AppRouter />
       </ThemeProvider>
     </>
